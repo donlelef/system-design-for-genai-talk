16e50da8b6203004a1eb1479ea21722ddefd32b8
Emanuele Fabbiani <donlelef@gmail.com>
2024-12-10 14:45:52 +0100

    refactor: format indexing.py

--- 01 - indexing/indexing.py
+++ 01 - indexing/indexing.py
@@ -14,16 +14,17 @@ def main():
     openai_client = OpenAI()
     embedding_model = "text-embedding-3-small"
     embedding_response = openai_client.embeddings.create(
-        input=movies_df["overview"].tolist(),
-        model=embedding_model
+        input=movies_df["overview"].tolist(), model=embedding_model
     )
-    movies_df['embedding'] = [e.embedding for e in embedding_response.data]
+    movies_df["embedding"] = [e.embedding for e in embedding_response.data]
 
     records = transform_movie_dataframe_to_records(movies_df)
     qdrant.upload_points(collection_name=collection_name, points=records)
 
 
-def transform_movie_dataframe_to_records(movies_df: pd.DataFrame) -> list[models.Record]:
+def transform_movie_dataframe_to_records(
+    movies_df: pd.DataFrame,
+) -> list[models.Record]:
     return [
         models.Record(
             id=idx,
@@ -40,24 +41,27 @@ def transform_movie_dataframe_to_records(movies_df: pd.DataFrame) -> list[models
     ]
 
 
-def delete_and_create_collection(qdrant: QdrantClient, collection_name: str, embedding_dimensions: int = 1536):
+def delete_and_create_collection(
+    qdrant: QdrantClient, collection_name: str, embedding_dimensions: int = 1536
+):
     if qdrant.collection_exists(collection_name):
         qdrant.delete_collection(collection_name)
 
     qdrant.create_collection(
         collection_name=collection_name,
         vectors_config=models.VectorParams(
-            size=embedding_dimensions,
-            distance=models.Distance.COSINE
+            size=embedding_dimensions, distance=models.Distance.COSINE
         ),
     )
 
 
 def fetch_movies() -> pd.DataFrame:
-    movies_df = pd.read_parquet("https://github.com/xtreamsrl/genai-for-engineers-class/raw/main/data/movies.parquet")
+    movies_df = pd.read_parquet(
+        "https://github.com/xtreamsrl/genai-for-engineers-class/raw/main/data/movies.parquet"
+    )
     movies_df = movies_df.iloc[:300, :]
     return movies_df
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
